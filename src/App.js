593192c084d2c816fe88c8b3c77fd9f1e2583090
Liam Roddy <you@example.com>
2024-08-07 15:32:05 +0100

    made mobile styling better, refactored structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Grid, Paper, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Container, useMediaQuery, useTheme, CssBaseline, Grid, Paper, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
-import doctorTopImage from './doctor_top.png';
-import doctorLegsImage from './doctor_legs.png';
-import peopleInCircleDiscussingLegsImage from './people-in-circle-discussing-legs.jpg';
-import talkingToDoctorImage from './talking-to-doctor.jpg';
-import doctorJeffImage from './doctor-jeff.jpg';
-import surgeonOnBoatImage from './surgeon-on-boat.jpg';
+import * as images from './images';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function ArticleSection({imageUri, imageAlt, sectionHeading, sectionText}) {
   return (
@@ -34,16 +34,62 @@ function ArticleSection({imageUri, imageAlt, sectionHeading, sectionText}) {
   );
 }
 
+function MainArticleContent() {
+  return (
+    <Grid container spacing={3}>
+      <ArticleSection 
+        sectionHeading="Need Legs?"
+        sectionText={<>
+                      <p>Do you have trouble reaching high shelves? Do you have two left feet, or none or at all? Or do you maybe just want a change of pace?</p>
+                      <p>Well you've come to the right site! </p>
+                      <p>Here at Doctor Jeff's Real Human Legs clinic we have available a wide range of new and pre-owned <a href="https://en.wikipedia.org/wiki/Leg">legs</a> in various lengths, colours and degrees of hairiness.</p>
+                    </>}
+        imageUri={images.peopleInCircleDiscussingLegsImage}
+        imageAlt="A group of people gathered in a circle, happily discussing legs."/>
+
+      <ArticleSection
+        sectionHeading="Sounds Kinda Dodgy..."
+        sectionText={<>
+                      <p>People often ask us:</p>
+                      <ul>
+                        <li>Is this legal?</li>
+                        <li>Is this ethical?</li>
+                        <li>Where are you getting these legs from?</li>
+                      </ul>
+                      <p>The answer to all these questions is <strong>YES!</strong></p></>}
+        imageUri={images.talkingToDoctorImage}
+        imageAlt={"A person talking to a doctor about their leg options."}/>
+
+      <ArticleSection
+        sectionHeading="Who is Doctor Jeff?"
+        sectionText={<>
+                      <p>Doctor Jeff is a medically aware person dedicated to getting you the best deal he can on legs!</p>                           
+                      <p>To show how dedicated he is to this noble task he even legally had his first name changed to Doctor!</p></>}
+        imageUri={images.doctorJeffImage}
+        imageAlt={"Doctor Jeff"}/>
+
+      <ArticleSection
+        sectionHeading="Where are We Based?"
+        sectionText={<>
+                      <p>Our clinic is based right in the heart of Galway City.
+                      <br></br>Your surgery, meanwhile, will be performed in the heart of a small fishing vessel fifteen kilometres offshore.</p>                           
+                      <p>Our services are not recommended for patients with seafood allergies.</p></>}
+        imageUri={images.surgeonOnBoatImage}
+        imageAlt={"Doctor Jeff"}/>
+    </Grid>
+            
+  );
+}
+
 function MainArticle() {
   return (
     <div>
       <Grid container spacing={0}>
         <Grid className="doctor-top-container" item xs={3} md={4}>
-          <img className='doctor-top-image' src={doctorTopImage} alt="A very trustworthy and not at all shady-looking doctor, photographed from the waist up, emerging from below text box." />          
+          <img className='doctor-top-image' src={images.doctorTopImage} alt="A very trustworthy and not at all shady-looking doctor, photographed from the waist up, emerging from below text box." />          
         </Grid>
         <Grid className="site-title" xs={9} md={8}>
           <div className="top-quote">
-            <p>We buy and sell</p>
             <h1>Real Human Legs</h1>
             <p>at prices that won't cost you an arm!</p>
             <p className='quote-signature'>- Doctor Jeff</p>
@@ -52,52 +98,12 @@ function MainArticle() {
 
         <Grid item xs={12} spacing={24}>
           <Paper className='main-article-paper' >
-            <Grid container spacing={3}>
-              <ArticleSection 
-                sectionHeading="Need Legs?"
-                sectionText={<>
-                              <p>Do you have trouble reaching high shelves? Do you have two left feet, or none or at all? Or do you maybe just want a change of pace?</p>
-                              <p>Well you've come to the right site! </p>
-                              <p>Here at Doctor Jeff's Real Human Legs clinic we have available a wide range of new and pre-owned <a href="https://en.wikipedia.org/wiki/Leg">legs</a> in various lengths, colours and degrees of hairiness.</p>
-                            </>}
-                imageUri={peopleInCircleDiscussingLegsImage}
-                imageAlt="A group of people gathered in a circle, happily discussing legs."/>
-
-              <ArticleSection
-                sectionHeading="Sounds Kinda Dodgy..."
-                sectionText={<>
-                              <p>People often ask us:</p>
-                              <ul>
-                                <li>Is this legal?</li>
-                                <li>Is this ethical?</li>
-                                <li>Where are you getting these legs from?</li>
-                              </ul>
-                              <p>The answer to all these questions is <strong>YES!</strong></p></>}
-                imageUri={talkingToDoctorImage}
-                imageAlt={"A person talking to a doctor about their leg options."}/>
-
-              <ArticleSection
-                sectionHeading="Who is Doctor Jeff?"
-                sectionText={<>
-                              <p>Doctor Jeff is a medically aware person dedicated to getting you the best deal he can on legs!</p>                           
-                              <p>To show how dedicated he is to this noble task he even legally had his first name changed to Doctor!</p></>}
-                imageUri={doctorJeffImage}
-                imageAlt={"Doctor Jeff"}/>
-
-              <ArticleSection
-                sectionHeading="Where are We Based?"
-                sectionText={<>
-                              <p>Our clinic is based right in the heart of Galway City.
-                              <br></br>Your surgery, meanwhile, will be performed in the heart of a small fishing vessel fifteen kilometres offshore.</p>                           
-                              <p>Our services are not recommended for patients with seafood allergies.</p></>}
-                imageUri={surgeonOnBoatImage}
-                imageAlt={"Doctor Jeff"}/>
-            </Grid>
+            <MainArticleContent />
           </Paper>
         </Grid>
 
         <Grid className="doctor-legs" item xs={3} md={4}>
-          <img className='doctor-legs-image' src={doctorLegsImage} alt="Doctor" />
+          <img className='doctor-legs-image' src={images.doctorLegsImage} alt="Doctor" />
         </Grid>
         <Grid className="empty-spacer" xs={9} md={8}>
         <footer>
@@ -113,16 +119,16 @@ function MainArticle() {
 }
 
 function App() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+  
   return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid className="main-body-spacer" item xs={0} md={2}></Grid>
-         <Grid  className="main-body-content" item xs={12} md={8}>
-          <MainArticle></MainArticle>
-          </Grid>
-        <Grid className="main-body-spacer" item xs={0} md={2}></Grid>
-      </Grid>
-    </div>
+    <>
+      <CssBaseline />
+      <Container style={{ paddingLeft: matches ? '8%' : '2%', paddingRight: matches ? '8%' : '2%' }}>
+        <MainArticle />
+      </Container>
+    </>
   );
 }
 
---
+++ src/images/index.js
@@ -0,0 +1,6 @@
+export { default as doctorTopImage } from './doctor_top.png';
+export { default as doctorLegsImage } from './doctor_legs.png';
+export { default as peopleInCircleDiscussingLegsImage } from './people-in-circle-discussing-legs.jpg';
+export { default as talkingToDoctorImage } from './talking-to-doctor.jpg';
+export { default as doctorJeffImage } from './doctor-jeff.jpg';
+export { default as surgeonOnBoatImage } from './surgeon-on-boat.jpg';
